36f8efc667d49f8ead666fb51dff0323e7e10276
Hüseyin Özlü <59911573+hus3y1n@users.noreply.github.com>
2021-05-29 18:59:04 +0300

    Delete hrms directory

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/EmployersService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.business.abstracts;
-
-import java.util.List;
-
-import kodlamaio.hrms.entities.concrates.Employers;
-
-public interface EmployersService {
-		List<Employers> getAll();
-}
--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/UnemployedService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.business.abstracts;
-
-import java.util.List;
-
-import kodlamaio.hrms.entities.concrates.Unemployed;
-
-public interface UnemployedService {
-		List<Unemployed> getAll();
-}
