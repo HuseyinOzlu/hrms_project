cbfa3d5b8ff9b4b07536d93644eee684e7dd3326
hüseyin özlü <hozlu509@gmail.com>
2021-05-29 18:32:23 +0300

    last commit

--- src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -12,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class HrmsApplication {
-	//Huseyin Ozlu  
+	//Huseyin Ozlü
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
