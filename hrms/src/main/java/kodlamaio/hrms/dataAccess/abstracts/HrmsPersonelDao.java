9ff7f534569457746f6adb041304dacc02ffb33a
Hüseyin Özlü <59911573+hus3y1n@users.noreply.github.com>
2021-06-02 20:38:03 +0300

    last commit

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/HrmsPersonelDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.hrms.entities.concrates.HrmsPersonel;
-
-public interface HrmsPersonelDao extends JpaRepository<HrmsPersonel, Integer>{
-
-}
