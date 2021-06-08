dba97c286a965c380ca009dacc18948e6a780bd7
Hüseyin Özlü <59911573+hus3y1n@users.noreply.github.com>
2021-06-08 18:20:07 +0300

    last commit

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concrates/Employers.java
@@ -0,0 +1,32 @@
+package kodlamaio.hrms.entities.concrates;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@Table(name ="employers")
+@EqualsAndHashCode(callSuper=false)
+@PrimaryKeyJoinColumn(name = "user_id")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employers extends Users {
+	
+	@Column(name="Company_name")
+	private String companyName;
+
+	@Column(name="Web_adress")
+	private String companyWebSite;
+	
+	@Column(name="Phone_number")
+	private String companyPhone;
+
+}
