dd9304ca026e652de312bacf9705d8be2c60b4b9
Hüseyin Özlü <59911573+hus3y1n@users.noreply.github.com>
2021-06-11 19:15:42 +0300

    Delete hrms directory

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/HrmsPersonelService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.business.abstracts;
-
-import java.util.List;
-
-import kodlamaio.hrms.entities.concrates.HrmsPersonel;
-
-public interface HrmsPersonelService {
-		List<HrmsPersonel> getAll();
-}
